1197c88c28f195bd38a03914aed67129602f8218
Shashwot Adhikari <shashwot123@gmail.com>
2025-05-22 21:59:44 +0545

    Add login functionality.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from accounts.views import SignupView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     #.as_view() converts class based view into a callable view
-    path("signup/", SignupView.as_view(), name="signup")
+    path("signup/", SignupView.as_view(), name="signup"),
+    #using django's LoginView
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 ]
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
